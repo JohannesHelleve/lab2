4de9b827561c195844b6e8093fb14656d66ddaeb
Johannes Helleve <Johanshelleve@gmail.com>
2022-02-02 13:42:57 +0100

    Completed assignment

--- src/main/java/INF101/lab2/Fridge.java
+++ src/main/java/INF101/lab2/Fridge.java
@@ -1,39 +1,62 @@
 package INF101.lab2;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 public class Fridge implements IFridge{
+    int max;
+    ArrayList<FridgeItem> fridgeItems;
 
+
+    public Fridge(){
+        fridgeItems = new ArrayList<>();
+        max = 20;
+    }
 
     @Override
     public int nItemsInFridge() {
-        int MAX = 20;
-        
-        return 0;
+        return fridgeItems.size();
     }
 
     @Override
     public int totalSize() {
-        return 0;
+        return max;
     }
 
     @Override
     public boolean placeIn(FridgeItem item) {
+        if(nItemsInFridge() < totalSize()){
+            fridgeItems.add(item);
+            return true;
+        }
         return false;
     }
 
     @Override
     public void takeOut(FridgeItem item) {
-
+        if(fridgeItems.contains(item)){
+            fridgeItems.remove(item);
+        }else{
+            throw new NoSuchElementException();
+        }
     }
 
     @Override
     public void emptyFridge() {
-
+        fridgeItems.clear();
     }
 
     @Override
     public List<FridgeItem> removeExpiredFood() {
-        return null;
+        ArrayList<FridgeItem> expiredItems = new ArrayList<>();
+
+        for(FridgeItem item : fridgeItems){
+            if(item.hasExpired()){
+                expiredItems.add(item);
+            }
+        }
+        fridgeItems.removeAll(expiredItems);
+        return expiredItems;
     }
 }
